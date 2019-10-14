6fdbea39866a311be9d4738f313f1a4ec43060b1
Leon J Mac <leonjmac@protonmail.ch>
2019-10-14 23:25:09 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,31 @@
-Refinery::Core::Engine.routes.append do
+Refinery::Core::Engine.routes.draw do
+  get '/system/images/*dragonfly', :to => Dragonfly.app(:refinery_images)
 
-  # Admin routes
-  namespace :image_slideshows, :path => '' do
-    namespace :admin, :path => Refinery::Core.backend_route do
-      resources :image_slideshows do
-        resources :image_slides, :except => :show do
-          collection do
-            post :update_positions
-          end
-        end
-        collection do
-          post :update_positions
-        end
+  namespace :admin, :path => Refinery::Core.backend_route do
+    resources :image_slideshows do
+      resources :image_slides, :except => :show do
+        get :insert, :on => :collection
       end
     end
   end
-
 end
+
+# Refinery::Core::Engine.routes.append do
+
+#   # Admin routes
+#   namespace :image_slideshows, :path => '' do
+#     namespace :admin, :path => Refinery::Core.backend_route do
+#       resources :image_slideshows do
+#         resources :image_slides, :except => :show do
+#           collection do
+#             post :update_positions
+#           end
+#         end
+#         collection do
+#           post :update_positions
+#         end
+#       end
+#     end
+#   end
+
+# end
